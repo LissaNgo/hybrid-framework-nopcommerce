a0f043db4beda044ba4885052e212c37f41da18b
LissaNgo <ntmhieu97@gmail.com>
2022-05-24 22:45:01 +0700

    Init automation framework

---
+++ testcases/com/notcommerce/user/User_01_Register_Login.java
@@ -0,0 +1,24 @@
+package com.notcommerce.user;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class User_01_Register_Login {
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void TC_01() {
+	}
+
+	@Test
+	public void TC_02() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
